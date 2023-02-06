1d2ee971d75488dd3c9561d473353362f827ec36
Alexbarile <alexbarile93fg@yahoo.it>
2023-02-06 18:15:06 +0100

    aggiunte info database.php

--- database.php
+++ database.php
@@ -18,6 +18,16 @@
 
     // OBJECT
 
+    $InfoObject_1 = new InfoObject('https://images.unsplash.com/photo-1647616350787-6428e907a7fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y3JvY2NhbnRpbmklMjBjYW5lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', 'Voliera Wilma in Legno', '184,99€', 'Uccello', 'Legno', 'M: L 83 x P 67 x H 153 cm');
+    $InfoObject_2 = new InfoObject('https://images.unsplash.com/photo-1647616350787-6428e907a7fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y3JvY2NhbnRpbmklMjBjYW5lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', 'Cartucce Filtranti per Filtro EasyCrystal', '2,29€', 'Pesce', 'Materiale espanso', 'ND');
 
+    $InfoObjectArray = [$InfoObject_1, $InfoObject_2];
+
+    // PLAY
+
+    $InfoPlay_1 = new InfoPlay('https://images.unsplash.com/photo-1647616350787-6428e907a7fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y3JvY2NhbnRpbmklMjBjYW5lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', 'Kong Classic', '13,49€', 'Cane', 'Galleggia e Rimbalza', '8,5 x 10cm');
+    $InfoPlay_2 = new InfoPlay('https://images.unsplash.com/photo-1647616350787-6428e907a7fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y3JvY2NhbnRpbmklMjBjYW5lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60', 'Topini di peluche Trixie', '4,99€', 'Gatto', 'Morbido con codina in corda', '8,5 x 10cm');
+
+    $InfoPlayArray = [$InfoPlay_1, $InfoPlay_2];
 
 ?>
